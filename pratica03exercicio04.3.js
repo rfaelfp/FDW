eaa5dd047e3f1c937fceda7f4a92e4cc26f161fa
Rafael Ferreira Pedrosa <rafael.pedrosa@supernova.com.br>
2019-09-18 18:14:02 -0300

    tentando linkar function ao css

--- pratica03exercicio04.3.js
+++ pratica03exercicio04.3.js
@@ -1,5 +1,5 @@
 function calculaLogaritimo () {
-    var num1, num2
+    var num1, num2, result 
 
     num1 = parseInt(prompt("Digite o primeiro número (logaritimando): "))
     if (num1 <= 0)
@@ -9,5 +9,6 @@ function calculaLogaritimo () {
         if (num2 <= 0)
         alert("O número deve ser maior que zero!")
         else
-        document.write (Math.log(num1) / Math.log(num2))
+        result = (Math.log(num1) / Math.log(num2))
+        document.write("<h1>Resultado: " + result + "</h1>")
 }
