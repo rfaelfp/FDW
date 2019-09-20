fe94dd9a6bedcc907f565f9efbeeb7cbf20622b6
Rafael Ferreira Pedrosa <rafael.pedrosa@supernova.com.br>
2019-09-20 18:33:01 -0300

    Pratica 03 Exercicio 04.6

---
+++ pratica03exercicio04.6.js
@@ -0,0 +1,8 @@
+var m, i, c
+
+m = parseint(prompt("Informe o montante: "))
+i = parseFloat(prompt("Informe a taxa de juros: "))
+c = parseint(prompt("Informe o numero de dias: "))
+
+m = c * (1 + i) * Math.pow(t)
+document.write(m)
