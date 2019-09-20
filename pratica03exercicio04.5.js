c90b24bbbc9ee72e1fc26e6d3c621ded6785d3ee
Rafael Ferreira Pedrosa <rafael.pedrosa@supernova.com.br>
2019-09-20 18:22:36 -0300

    Prática 03 exercíciio 04.5

---
+++ pratica03exercicio04.5.js
@@ -0,0 +1,6 @@
+function calculaVolumeEsfera () {
+    raio = parseFloat(prompt("Informe o valor do raio: "))
+
+    esfera = 4 / 3 * Math.PI * Math.pow(raio, 3)
+    document.write("O valor é: " + esfera)
+}
