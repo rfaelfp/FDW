7a09e9a5a803c5823ca74fb8a5a69f25fd3c52d4
Rafael Ferreira <rafaelfepedrosa@gmail.com>
2019-09-22 18:22:26 -0300

    Alteraçõe nas práticas

--- pratica03exercicio04.6.js
+++ pratica03exercicio04.6.js
@@ -1,8 +1,17 @@
-var m, i, c
-
-m = parseint(prompt("Informe o montante: "))
-i = parseFloat(prompt("Informe a taxa de juros: "))
-c = parseint(prompt("Informe o numero de dias: "))
-
-m = c * (1 + i) * Math.pow(t)
-document.write(m)
+function calculaJurosComposto () {
+    c = parseInt(prompt("Informe o capital inicial: "))
+    if (c <= 0 | c == NaN)
+        alert("O número tem que ser inteiro!")
+    else
+        t = parseInt(prompt("Informe o numero de dias: "))
+            if (t <= 0 | t == NaN)
+                alert("O número tem que ser inteiro!")
+            else 
+                i = parseFloat(prompt("Informe a taxa de juros: "))
+                if (i <= 0 | i == NaN)
+                    alert("O número tem que ser inteiro!")
+                else
+                    i = i / 100
+                    m = c * Math.pow((1 + i), t)
+                    document.write("O resultado é " + m)
+}
