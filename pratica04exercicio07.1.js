9d2a7f33cea240bacfee074ae0e75c505b04606f
Rafael Ferreira <rafaelfepedrosa@gmail.com>
2019-10-03 17:55:15 -0300

    03/10/2019

--- pratica04exercicio07.1.js
+++ pratica04exercicio07.1.js
@@ -1,11 +1,10 @@
 var i, n = parseInt(prompt("Informe um número"))
 var soma = 0
+var somatotal
 
 for (i = 0; i <= n; i += 3)
     document.write(i + ", ")
-for (i = 0; i <= n; i += 3)
-    soma = soma + i
-document.write("= " + soma)
+
 
 document.write("<br>")
 var soma = 0
@@ -13,9 +12,6 @@ console.log(("Multiplos de 5 no intervalo [0, " + n + "]:"))
 
 for (i = 0; i <= n; i += 5)
     document.write(i + ", ")
-for (i = 0; i <= n; i += 5)
-    soma = soma + i
-document.write("= " + soma)
 
 document.write("<br>")
 var soma = 0
@@ -23,9 +19,7 @@ console.log(("Potência base 2 no intervalo [0, " + n + "]:"))
 
 for (i = 0; i <= n; i++)
     document.write(Math.pow(2, i) + ", ")
-for (i = 0; i <= n; i++)
-    soma = soma + Math.pow(2, i++)
-document.write("= " + soma)
+
 
 document.write("<br>")
 var soma = 0
@@ -33,6 +27,23 @@ console.log(("Potência base Variável no intervalo [0, " + n + "]:"))
 
 for (i = 0; i <= n; i++)
     document.write(Math.pow(i, 3) + ", ")
+
+
+for (i = 0; i <= n; i += 3)
+    soma = soma + i
+document.write("= " + soma)
+
+for (i = 0; i <= n; i += 5)
+    soma = soma + i
+document.write("= " + soma)
+
+for (i = 0; i <= n; i++)
+    soma = soma + Math.pow(2, i++)
+document.write("= " + soma)
+
 for (i = 0; i <= n; i++)
     soma = soma + Math.pow(i, 3)
 document.write("= " + soma)
+
+somatotal = soma
+document.write(somatotal)
