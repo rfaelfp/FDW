e3a5f7008e18d4f2aad9cdbcc22a9b6738f5f35f
Rafael Ferreira <rafaelfepedrosa@gmail.com>
2019-10-10 15:35:15 -0300

    10/10/2019

--- pratica07exercicio01.js
+++ pratica07exercicio01.js
@@ -1,9 +1,41 @@
-function myFunction () {
-    var total = 0 
-    var falta = 0
-    total = document.getElementsByClassName("total").value
-    falta = document.getElementsByClassName("falta").value
-    if (total <= 70)
-    document.write("Reprovado")
-    if
+var total, falta
+function aprovadoReprovado () {
+    total = document.getElementById("total").innerText
+    falta = document.getElementById("faltas").innerText
+    if (total < 70 || falta >= 3)
+    document.getElementById("resultado").innerHTML = "Reprovado"
+    else
+    document.getElementById("resultado").innerHTML = "Aprovado"
+}
+function aprovadoReprovado1 () {
+    total = document.getElementById("total1").innerText
+    falta = document.getElementById("faltas1").innerText
+    if (total < 70 || falta >= 3)
+    document.getElementById("resultado").innerHTML = "Reprovado"
+    else
+    document.getElementById("resultado").innerHTML = "Aprovado"
+}
+function aprovadoReprovado2 () {
+    total = document.getElementById("total2").innerText
+    falta = document.getElementById("faltas2").innerText
+    if (total < 70 || falta >= 3)
+    document.getElementById("resultado").innerHTML = "Reprovado"
+    else
+    document.getElementById("resultado").innerHTML = "Aprovado"
+}
+function aprovadoReprovado3 () {
+    total = document.getElementById("total3").innerText
+    falta = document.getElementById("faltas3").innerText
+    if (total < 70 || falta >= 3)
+    document.getElementById("resultado").innerHTML = "Reprovado"
+    else
+    document.getElementById("resultado").innerHTML = "Aprovado"
+}
+function aprovadoReprovado4 () {
+    total = document.getElementById("total4").innerText
+    falta = document.getElementById("faltas4").innerText
+    if (total < 70 || falta >= 3)
+    document.getElementById("resultado").innerHTML = "Reprovado"
+    else
+    document.getElementById("resultado").innerHTML = "Aprovado"
 }
---
+++ pratica07exercicio02.js
@@ -0,0 +1,11 @@
+var ana = {
+    nascimento: ["01/02/1980"],
+    endereco: ["Rua Augusto de Lima, 479 - Centro"],
+    email: ["ana@gmail.com"],
+    celular: ["(31)99923-5498"]
+}
+
+function ana () {
+document.getElementById("resultado").innerHTML
+}
+//nascimento, endereço, e-mail e celular 
