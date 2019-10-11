af7c46f4da2929bc0789d50dc64494314dd88db8
Rafael Ferreira <rafaelfepedrosa@gmail.com>
2019-10-11 12:14:20 -0300

    11/10/2019

--- pratica07exercicio01.js
+++ pratica07exercicio01.js
@@ -1,41 +1,6 @@
-var total, falta
-function aprovadoReprovado () {
-    total = document.getElementById("total").innerText
-    falta = document.getElementById("faltas").innerText
-    if (total < 70 || falta >= 3)
-    document.getElementById("resultado").innerHTML = "Reprovado"
-    else
+function aprovadoReprovado (param) {
+    if(param.getElementsByClassName("total")[0].innerHTML >= 70 && param.getElementsByClassName("faltas")[0].innerHTML < 3)
     document.getElementById("resultado").innerHTML = "Aprovado"
-}
-function aprovadoReprovado1 () {
-    total = document.getElementById("total1").innerText
-    falta = document.getElementById("faltas1").innerText
-    if (total < 70 || falta >= 3)
-    document.getElementById("resultado").innerHTML = "Reprovado"
     else
-    document.getElementById("resultado").innerHTML = "Aprovado"
-}
-function aprovadoReprovado2 () {
-    total = document.getElementById("total2").innerText
-    falta = document.getElementById("faltas2").innerText
-    if (total < 70 || falta >= 3)
-    document.getElementById("resultado").innerHTML = "Reprovado"
-    else
-    document.getElementById("resultado").innerHTML = "Aprovado"
-}
-function aprovadoReprovado3 () {
-    total = document.getElementById("total3").innerText
-    falta = document.getElementById("faltas3").innerText
-    if (total < 70 || falta >= 3)
-    document.getElementById("resultado").innerHTML = "Reprovado"
-    else
-    document.getElementById("resultado").innerHTML = "Aprovado"
-}
-function aprovadoReprovado4 () {
-    total = document.getElementById("total4").innerText
-    falta = document.getElementById("faltas4").innerText
-    if (total < 70 || falta >= 3)
-    document.getElementById("resultado").innerHTML = "Reprovado"
-    else
-    document.getElementById("resultado").innerHTML = "Aprovado"
+        document.getElementById("resultado").innerHTML = "Reprovado"
 }
--- pratica07exercicio03.js
+++ pratica07exercicio03.js
@@ -1,5 +1,19 @@
-var infor, infor2
-function notaMedia () {
-    infor = document.getElementsByClassName("total")[0].innerHTML
-    document.write(infor)
+function notaMedia (param) {
+    diferenca = (param.getElementsByClassName("total")[0].innerHTML - document.getElementById("mediaweb").innerHTML)
+    document.getElementById("resultadomedia").innerHTML = "A diferença da média com o total é " + diferenca.toFixed(2)
+    media = document.getElementById("mediaweb").innerHTML
+    if (param.getElementsByClassName("total")[0].innerHTML <= media)
+        document.getElementById("resultado").innerHTML = "Aluno com nota abaixo da média"
+    else
+        document.getElementById("resultado").innerHTML = "Aluno com nota acima da média"
 }
+function notaMedia2 (param) {
+    diferenca = (param.getElementsByClassName("total")[0].innerHTML - document.getElementById("mediaProgramacao").innerHTML)
+    document.getElementById("resultadomedia").innerHTML = "A diferença da média com o total é " + diferenca.toFixed(2)
+    media = document.getElementById("mediaProgramacao").innerHTML
+    if (param.getElementsByClassName("total")[0].innerHTML <= media)
+        document.getElementById("resultado").innerHTML = "Aluno com nota abaixo da média"
+    else
+        document.getElementById("resultado").innerHTML = "Aluno com nota acima da média"
+}
+
