b9cb81d92485c5a21dd893627d13d9fb0d89639e
Rafael Ferreira <rafaelfepedrosa@gmail.com>
2019-11-12 00:02:45 -0200

    11/11/1029

---
+++ pratica09exercicio01.js
@@ -0,0 +1,25 @@
+function verificaFormulario () {
+    var formulario = document.forms["formularioCadastro"]
+    var senha = formulario.senha.value
+    var contraSenha = formulario.contraSenha.value
+    var sexo = formulario.sexo.value
+
+    if (senha != contraSenha) {
+        alert("Senhas diferentes")
+        return false }
+    if (sexo == "selecione") {
+        alert("Gentileza escolher o sexo")
+        return false
+    }
+}
+
+function reescrever () {
+    if (site.value == "")
+    site.value = "https://"
+}
+
+
+function limpar() {
+    document.getElementsByClassName("limpar").value = ""
+
+}
