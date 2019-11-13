dd2be37cdb96bd1f7e94b5663004f4fa5fdedd4d
Rafael Ferreira Pedrosa <rafael.pedrosa@supernova.com.br>
2019-11-13 20:20:53 -0300

    13/11/2019

--- pratica10exercicio01.js
+++ pratica10exercicio01.js
@@ -1,16 +1,33 @@
 function esconderInfo () {
     info.style.visibility = "hidden";
 }
-function mostrarInfo () {
+
+function mostrarInfo (opcao) {
+    var descricao
     info.style.visibility = "visible";
     window.location.href='#logo'
-}
 
-switch (key) {
-    case value:
-        
+switch (opcao) {
+    case "rio":
+        descricao = "Informações do Rio de Janeiro. Orçamento do Rio de Janeiro."
         break;
-
+    case "bali":
+        descricao = "Informações de Bali. Orçamento de Bali."
+        break;
+    case "londres":
+        descricao = "Informações de Londres. Orçamento de Londres."
+        break;
+    case "sydney":
+        descricao = "Informações de Sydney. Orçamento de Sydney."
+        break;
+    case "lisboa":
+        descricao = "Informações de Lisboa. Orçamento de Lisboa."  
+        break;
+    case "amsterdam":
+        descricao = "Informações de Amsterdam. Orçamento de Amsterdam."
+        break;
     default:
         break;
+}
+    info2.innerHTML = descricao
 }
