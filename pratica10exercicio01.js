5102d952887b7510ded632986d1e6e4b80cd42ca
Rafael Ferreira Pedrosa <rafael.pedrosa@supernova.com.br>
2019-11-13 18:34:09 -0300

    13/11/2019

---
+++ pratica10exercicio01.js
@@ -0,0 +1,16 @@
+function esconderInfo () {
+    info.style.visibility = "hidden";
+}
+function mostrarInfo () {
+    info.style.visibility = "visible";
+    window.location.href='#logo'
+}
+
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
