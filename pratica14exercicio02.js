2a39501cfcd82702c196e79c74ddfcbe78da65d0
Rafael Ferreira Pedrosa <rafael.pedrosa@supernova.com.br>
2019-11-22 10:32:10 -0300

    22/11/2019

--- pratica14exercicio02.js
+++ pratica14exercicio02.js
@@ -1,10 +1,5 @@
 $(document).ready(function () {
     $("#botao").click(function (){
-        if ($("div").hasClass("container")){
-            $("img").hide()
-            $("div").removeClass("container").addClass("embranco")
-        }        
-
-
+        $("img").toggle()
     })
 })
